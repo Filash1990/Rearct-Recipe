6931790e84c0075d8fd43e59a3e357b2fac34a81
Filas Siuma <52425572+Filash1990@users.noreply.github.com>
2019-07-09 15:57:58 +0300

    first upload

---
+++ build/precache-manifest.d820d592f9e3130a5eaeafaa910bd74f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b9f00e72b3fa1c4fd9394082fed9cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "541956cf991661fc411f",
+    "url": "/static/css/main.91c9c673.chunk.css"
+  },
+  {
+    "revision": "d27ba6dadf7b6d6d23f2",
+    "url": "/static/js/2.aa9b191f.chunk.js"
+  },
+  {
+    "revision": "541956cf991661fc411f",
+    "url": "/static/js/main.4e5b1c83.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
